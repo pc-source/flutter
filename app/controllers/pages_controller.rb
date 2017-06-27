c8c36e3d16bb11ef4d7ffb5c5cfafd77c1027915
sherlockpc27 <sherlockpc27@gmail.com>
2017-06-27 00:32:09 -0400

    Made posts controller and new posts in profile.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
+# The pages controller contains all of the code for any page inside of /pages
 class PagesController < ApplicationController
+  
   # back-end code for pages/index
   def index
   end
 
   # back-end code for pages/home
   def home
+  @posts = Post.all
   end
 
   # back-end code for pages/profile
@@ -16,9 +19,18 @@ class PagesController < ApplicationController
       # redirect to 404 (root for now)
       redirect_to root_path, :notice=> "User not found!" 
     end
+    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
+    
+    
+   
   end
 
   # back-end code for pages/explore
   def explore
+    @posts = Post.all
+   
   end
+  
 end
